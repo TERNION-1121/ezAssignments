08c616604bfe781d16e0358742e5c5152019ff29
Vikrant Singh Bhadouriya <97667653+TERNION-1121@users.noreply.github.com>
2024-02-29 18:35:26 +0530

    Complete minimal assignment.docx generator TODOs: Error handling, more flexibility in document customization etc.

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from src.assignment import Assignment
 
 assignment = Assignment("assignment.txt")
+assignment.InitDoc()
+assignment.AddMCQS()
+assignment.AddARS()
+assignment.AddSUBS()
--- src/assignment.py
+++ src/assignment.py
@@ -1,3 +1,5 @@
+import docx
+
 from src.question_classes import *
 from src.utils import *
 
@@ -24,11 +26,11 @@ class Assignment:
         end = len(contents) - 1
         while contents[end] == '\n':
             end -=1
-        
+        # split the questions
         contents = split_contents(contents[start:end+1])
-
+        # stores the questions as class objects
         self.QUESTIONS = {"MCQS": [], "ARS": [], "SUBS": [], "INVALIDATED": []}
-
+        # process each question
         for question in contents:
 
             match question[0]:
@@ -41,3 +43,59 @@ class Assignment:
                     self.QUESTIONS["SUBS"].append(SUB(question[1:]))
                 case _:
                     self.QUESTIONS["INVALIDATED"].append(question)
+        
+        self.doc = docx.Document()
+
+
+    def InitDoc(self):
+        self.doc.add_heading("Assignment", level=0)
+        self.doc.save("assignment.docx")
+    
+
+    def AddMCQS(self):
+        self.doc.add_heading("Multiple Choice Questions", level=1)
+
+        for mcq in self.QUESTIONS["MCQS"]:
+            # add statement
+            statement = ' '.join(mcq.question)
+            self.doc.add_paragraph(statement)
+            # add choices
+            for opt in mcq.options:
+                self.doc.add_paragraph(f"{opt}: {mcq.options[opt]}", style="List 2")
+            # newline
+            self.doc.add_paragraph()
+
+        self.doc.save("assignment.docx")
+
+
+    def AddARS(self):
+        self.doc.add_heading("Assertion and Reasoning", level=1)
+        # add instruction
+        instruction = self.doc.add_paragraph()
+        instruction.add_run("Using the given option choices, choose the most suitable one ")
+        instruction.add_run("to answer the following questions.")
+        # add choices
+        for opt in AR.OPTIONS:
+            self.doc.add_paragraph(f"{opt}: {AR.OPTIONS[opt]}", style="List 2")
+        self.doc.add_paragraph() # newline
+
+        for ar in self.QUESTIONS["ARS"]:
+            # add statements
+            assertion = ' '.join(ar.assertion)
+            reason = ' '.join(ar.reason)
+            self.doc.add_paragraph("Assertion (A): ").add_run(assertion)
+            self.doc.add_paragraph("Reason (R): ").add_run(reason)
+            self.doc.add_paragraph()
+
+        self.doc.save("assignment.docx")
+
+
+    def AddSUBS(self):
+        self.doc.add_heading("Subjective Questions", level=1)
+        # add statement
+        for sub in self.QUESTIONS["SUBS"]:
+            statement = ' '.join(sub.question)
+            self.doc.add_paragraph(statement)
+            self.doc.add_paragraph()
+        
+        self.doc.save("assignment.docx")

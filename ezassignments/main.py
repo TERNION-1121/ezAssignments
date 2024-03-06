81d6a9893122efdce5625d2db3eeb85a521902fe
Vikrant Singh Bhadouriya <97667653+TERNION-1121@users.noreply.github.com>
2024-03-06 21:22:52 +0530

    Update code structure

--- ezassignments/main.py
+++ ezassignments/main.py
@@ -21,29 +21,27 @@ def main():
                         default="assignment")
     args = parser.parse_args()
 
-    assignment_doc = Assignment(args.dir_path, args.txt_file_name, args.docxname)
+    assignment_doc = Assignment(args.dir_path, args.txtname, args.docname)
+
     # ask for optional instructions for each question type
-    for question_type in assignment_doc.QUESTIONS.keys():
-        if question_type == "INVALIDATED":
-            break
-        question_type.process_instructions()
+    for q_type in assignment_doc.QUESTION_CLS:
+        q_type.process_instructions()
 
     assignment_doc.init_doc()
 
-    if assignment_doc.QUESTIONS[MCQ]:
+    if MCQ.QUESTIONS:
         assignment_doc.add_MCQS()
-    if assignment_doc.QUESTIONS[AR]:
+    if AR.QUESTIONS:
         assignment_doc.add_ARS()
-    if assignment_doc.QUESTIONS[SUB]:
+    if SUB.QUESTIONS:
         assignment_doc.add_SUBS()
-    if not assignment_doc.QUESTIONS["INVALIDATED"]:
+    if not Invalid.QUESTIONS:
         return 
     
     with open(f"{args.dir_path}/invalidated_questions.txt", 'w') as file:
         file.write("THE FOLLOWING QUESTIONS WERE INVALIDATED:\n\n")
-        for question in assignment_doc.QUESTIONS["INVALIDATED"]:
-            for line in question:
-                file.write(line + '\n')
+        for question in Invalid.QUESTIONS:
+            file.writelines(question.statement)
             file.write('\n')
 
 

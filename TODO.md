3a919ffaa192c1ae57ef21f21e890c504657bd04
Vikrant Singh Bhadouriya <97667653+TERNION-1121@users.noreply.github.com>
2024-03-06 15:09:47 +0530

    Add functionality to write question instructions

--- TODO.md
+++ TODO.md
@@ -1,5 +1,6 @@
+# TODOS
+
 - Add optional subheading functionality
 - Image functionality
 - Add edit heading feature
 - Add new question type Case Studies
-- Functionality to add optional question instructions
--- ezassignments/src/question_classes.py
+++ ezassignments/src/question_classes.py
@@ -1,12 +1,72 @@
+import docx
+
+
 class Question:
-    def __init__(self, question: list[str]) -> None:
+    INSTRUCTIONS = None
+
+    def __init__(self, question: list[str] | None) -> None:
         self.question = question
+
+    @classmethod
+    def process_instructions(cls) -> None:
+        """
+        Prompt the user to process optional instructions in the main program
+        """
+        yes_opts = ('y', 'yes')
+        no_opts = ('n', 'no')
+        choice = None
+
+        while choice not in yes_opts + no_opts:
+            print(f"Add instructions for {cls.__name__}? (y/n):", end=" ")
+            choice = input().strip().lower()
+
+        if choice in no_opts:
+            cls.INSTRUCTIONS = None
+            return
+
+        if cls.INSTRUCTIONS is not None:
+            print(f"Default instructions for {cls.__name__}:")
+            print(''.join(cls.INSTRUCTIONS))
+
+            choice = None
+            while choice not in yes_opts + no_opts:
+                print("Continue with default instructions? (y/n):", end=" ")
+                choice = input().strip().lower()
+
+            if choice in yes_opts:
+                return
+
+        print("Write your instructions (enter a blank newline to stop giving input):")
+        cls.INSTRUCTIONS = list()
+        while inp := input().strip():
+            cls.INSTRUCTIONS.append(f"{inp}\n")
+
+    @classmethod
+    def write_instructions(cls, doc: docx.Document) -> None:
+        if not cls.INSTRUCTIONS:
+            return
+
+        instruction = doc.add_paragraph()
+        for line in cls.INSTRUCTIONS:
+            instruction.add_run(line)
 
 
 class MCQ(Question):
-    def __init__(self, question: list[str], options: dict[str:str]) -> None:
+    INSTRUCTIONS = None
+
+    def __init__(self,
+                 question: list[str],
+                 options: dict[str:str]) -> None:
         super().__init__(question)
         self.options = options
+
+    @classmethod
+    def process_instructions(cls) -> None:
+        super(MCQ, cls).process_instructions()
+
+    @classmethod
+    def write_instructions(cls, doc: docx.Document) -> None:
+        super(MCQ, cls).write_instructions(doc)
 
     @classmethod
     def from_list_content(cls, content: list[str]):
@@ -25,7 +85,9 @@ class MCQ(Question):
         return f"""MCQ({self.question[0][:10]}...{self.question[-1][-10:]})"""
 
 
-class AR:
+class AR(Question):
+    INSTRUCTIONS = ["The following questions consist of two statements- Assertion (A) and Reasoning (R)\n",
+                    "Answer these questions selecting the most appropriate option given below:"]
     OPTIONS = {'A': "Both Assertion (A) and Reason (R) are True, and R is the correct explanation of A.",
                'B': "Both Assertion (A) and Reason (R) are True, but R is not the correct explanation of A.",
                'C': "Assertion (A) is True, Reason (R) is False.",
@@ -33,9 +95,20 @@ class AR:
                'E': "Both Assertion (A) and Reason (R) are False"
                }
 
-    def __init__(self, assertion: list[str], reason: list[str]) -> None:
+    def __init__(self,
+                 assertion: list[str],
+                 reason: list[str]) -> None:
+        super().__init__(None)
         self.assertion = assertion
         self.reason = reason
+
+    @classmethod
+    def process_instructions(cls) -> None:
+        super(AR, cls).process_instructions()
+
+    @classmethod
+    def write_instructions(cls, doc: docx.Document) -> None:
+        super(AR, cls).write_instructions(doc)
 
     @classmethod
     def from_list_content(cls, content: list[str]):
@@ -54,8 +127,18 @@ class AR:
 
 
 class SUB(Question):
+    INSTRUCTIONS = None
+
     def __init__(self, question: list[str]) -> None:
         super().__init__(question)
+
+    @classmethod
+    def process_instructions(cls) -> None:
+        super(SUB, cls).process_instructions()
+
+    @classmethod
+    def write_instructions(cls, doc: docx.Document) -> None:
+        super(SUB, cls).write_instructions(doc)
 
     def __repr__(self) -> str:
         return f"""SUB({self.question[0][:10]}...{self.question[-1][-10:]})"""
